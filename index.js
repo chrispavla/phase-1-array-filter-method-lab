e8c5813b99f683eaf1bad2041a65e24758c1d7db
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-23 18:55:32 -0400

    commit tree 8b2043f3ef3e785f7f38dfb00c34953d769476fb

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+// function findMatching(drivers, matchedDriver) {
+
+//     return drivers.filter(function(driver) {
+
+//         return driver.toLowerCase() === matchedDriver.toLowerCase()})
+// }
+
+// function fuzzyMatch(drivers, argument) {
+
+//     return drivers.filter(function(driver) {
+
+//         return driver.toLowerCase().substring(0, argument.length) === argument.toLowerCase()
+//     })
+
+// }
+
+// function matchName(drivers, argument) {
+//     return drivers.filter(function(driver) {
+
+//         return driver.name === argument;
+//     })
+// }
+
+function findMatching(arr, str) {
+  return arr.filter((driver) => driver.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter((driver) => driver.slice(0, 2) === str);
+}
+
+function matchName(arr, str) {
+  return arr.filter((driver) => driver.name === str);
+}
+
+// let drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah"];
+// console.log(findMatching(drivers, "bobby"));
